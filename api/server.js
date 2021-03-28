c61252e0048b7fbb89a117756d93d5a0639bb033
Josh <josh.walters@outlook.com>
2021-03-28 08:52:57 -0500

    Added jokes api (#1)
    * added jokes.json
    updated api endpoint for jokes
    updated vue to get jokes
    
    * added travis.yml
    
    * added "main" branch to travis.yml
    
    * added app name for heroku deploy
    
    * updated test scripts
    
    * added main branch to heroku deployment in travis.yml

--- api/server.js
+++ api/server.js
@@ -1,19 +1,30 @@
 // Require packages and set the port
 const express = require('express');
+const path = require('path');
 const body_parser = require('body-parser');
 const cors = require('cors')
-const port = 3800;
+const PORT = process.env.PORT || 8080;
+const HOST = PORT === 8080 ? 'localhost' : '0.0.0.0';
+
 const app = express();
 
 app.use(body_parser.json());
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, '../app/dist')));
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'Connected!' });
+app.get('/api/joke', (req, res, next) => {
+  const jokes = require('./jokes.json');
+  const joke = jokes[Math.floor(Math.random() * jokes.length)];
+  res.status(200).json(joke);
 });
 
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../app/dist', 'index.html'))
+});
+
+
 // Start the server
-app.listen(process.env.PORT || 4242, function(){
-  console.log('Your node js server is running');
+app.listen(PORT, HOST, function(){
+  console.log(`Node js server is listening on ${ PORT }`);
 });
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@
         mockpuppets.com
       </h1>
       <div class="links">
-        <a
+        <!-- <a
           href="https://nuxtjs.org/"
           target="_blank"
           rel="noopener noreferrer"
@@ -21,14 +21,61 @@
           class="button--grey"
         >
           GitHub
-        </a>
+        </a> -->
+        <a href="#" class="button--green" @click="mock()">Mock</a>
+      </div>
+      <div class="main-content">
+        <div class="joke" v-if="!!joke.setup">
+          <div class="setup">{{ setup }}</div>
+          <div class="punchline" v-show="joke.punchlineIsVisible">{{ joke.punchline }}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      joke: { setup: '', punchline: '', punchlineIsVisible: false },
+    };
+  },
+  computed: {
+    setup() {
+      if (!this.joke || !this.joke.setup)
+        return '';
+      const questionWords = ['who', 'what', 'where', 'when', 'why', 'how'];
+      if (questionWords.some(x => this.joke.setup.toLowerCase().startsWith(x))) {
+        if (this.joke.setup.indexOf('?') < 0)
+          return this.joke.setup + '?';
+      }
+      return this.joke.setup;
+    },
+  },
+  methods: {
+    mock() {
+      return fetch('/api/joke')
+        .then(r => r.json())
+        .then(r => {
+          if (this.joke.setup === r.setup) {
+            this.mock();
+          } else {
+            this.joke = {...r, punchlineIsVisible: false };
+          }
+        });
+    }
+  },
+  watch: {
+    'joke': function (oldVal, newVal) {
+      if (!!newVal && newVal.setup !== this.joke.setup) {
+        setTimeout(() => {
+          this.$set(this.joke, 'punchlineIsVisible', true);
+        }, 2500);
+      }
+    }
+  }
+}
 </script>
 
 <style>
@@ -45,7 +92,10 @@ export default {}
   align-items: center;
   text-align: center;
 }
-
+.main-content {
+  padding-top: 1rem;
+  min-height: 200px;
+}
 .title {
   font-family:
     'Quicksand',
